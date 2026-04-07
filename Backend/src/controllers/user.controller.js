191811cc4980e39a63ae061d470cfb96d4335a59
satyamtiwari-1499 <satyamtiwari1499@gmail.com>
2026-04-07 11:18:27 +0530

    fixed done

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 export const orderplaced = asyncHandler(async (req, res) => {
     let user = req.user;
     const {email}=user
-    let mailsend = await sendmail(email, "Marthon Oredr status", `Dear ${user.userName}, Your OredrPlaced Successfully!`);
+    // let mailsend = await sendmail(email, "Marthon Oredr status", `Dear ${user.userName}, Your OredrPlaced Successfully!`);
     res.status(200).json({
         success: true,
         message:" Your OredrPlaced Successfully!, check confirmation mail"
